08f420f4ee25629df9c553e501814152f771041e
SeniorZhai <developer.zhaitao@gmail.com>
2019-05-19 17:05:21 +0800

    Build in activity

--- android/app/src/main/java/io/seniorzhai/hybrid/MainActivity.kt
+++ android/app/src/main/java/io/seniorzhai/hybrid/MainActivity.kt
@@ -1,12 +1,26 @@
 package io.seniorzhai.hybrid
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import io.flutter.facade.Flutter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start.setOnClickListener {
+            buildFlutter()
+        }
+    }
+
+    private val flutterView by lazy {
+        Flutter.createView(this,lifecycle,null)
+    }
+
+    private fun buildFlutter(){
+        container.addView(flutterView,FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT))
     }
 }
