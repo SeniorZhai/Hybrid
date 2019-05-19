e49ce12dbdba8399b9dfd2b04614277806f68131
SeniorZhai <developer.zhaitao@gmail.com>
2019-05-19 22:11:18 +0800

    Flutter call native method

--- android/app/src/main/java/io/seniorzhai/hybrid/MainActivity.kt
+++ android/app/src/main/java/io/seniorzhai/hybrid/MainActivity.kt
@@ -1,10 +1,16 @@
 package io.seniorzhai.hybrid
 
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.facade.Flutter
+import io.flutter.plugin.common.MethodChannel
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +23,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val flutterView by lazy {
-        Flutter.createView(this,lifecycle,null)
+        Flutter.createView(this, lifecycle, null)
     }
 
-    private fun buildFlutter(){
-        container.addView(flutterView,FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT))
+    private fun buildFlutter() {
+        MethodChannel(flutterView, "seniorzhai.io").setMethodCallHandler { methodCall, result ->
+            if (methodCall.method.equals("getBatteryLevel")) {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    // 成功
+                    result.success(batteryLevel)
+                } else {
+                    // 失败
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                // 表示没有对应实现
+                result.notImplemented()
+            }
+        }
+        container.addView(flutterView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
+    }
+
+    private fun getBatteryLevel(): Int {
+        val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        return intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
     }
 }
--- hybrid/lib/main.dart
+++ hybrid/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,27 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _content = "None";
+
+  @override
+  void initState() {
+    _getBatteryLevel();
+  }
+
+  static const MethodChannel methodChannel = MethodChannel('seniorzhai.io');
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await methodChannel.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level: $result%.';
+    } on PlatformException {
+      batteryLevel = 'Failed to get battery level.';
+    }
+    setState(() {
+      _content = batteryLevel;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
